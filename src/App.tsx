7b89259b86d2d678cd7fa54eb0f46c2143af7419
Sowmya <sowmya.manu@yahoo.com>
2022-04-11 21:54:34 +0100

    Created the id for all cats by install uuid,and also tried by importing v4 as uuidv4

--- src/App.tsx
+++ src/App.tsx
@@ -5,89 +5,13 @@ import Footer from './components/footer';
 import  { useState } from 'react';
 import Cat from './data/cat';
 import CatCard from './components/cat_card'
+import catData from  './data/catData'
+
+
 
 function App() {
 	// JavaScript code can be inserted here!
-	const [cats,setCats] = useState<Array<Cat>>([
-		{
-				 name: 'Little Miss Purrfect',
-				 species: 'Cat',
-				 favFoods: ['wet food', 'dry food'],
-				 birthYear: 2016,
-			 },
-			 {
-				 name: 'Mr Gwumpy',
-				 species: 'Cat',
-				 favFoods: ['caviar'],
-				 birthYear: 2016,
-			 },
-			 {
-				 name: 'Dopey',
-				 species: 'Cat',
-				 favFoods: ['bugs'],
-				 birthYear: 2018,
-			 },
-			 {
-				 name: 'Jumpy',
-				 species: 'Scaredy Cat',
-				 favFoods: ['not cucumbers!'],
-				 birthYear: 2008,
-			 },
-			 {
-				 name: 'Meowsalot',
-				 species: 'Cat',
-				 favFoods: ['tuna', 'catnip', 'celery'],
-				 birthYear: 2012,
-			 },
-			 {
-				 name: 'Cindy Clawford',
-				 species: 'Cat',
-				 favFoods: ['mice'],
-				 birthYear: 2012,
-			 },
-			 {
-				 name: 'Katy Purry',
-				 species: 'Cat',
-				 favFoods: ['cigarettes', 'coffee'],
-				 birthYear: 2015,
-			 },
-			 {
-				 name: 'Dr. Von Belly-Rub',
-				 species: 'Cat',
-				 favFoods: ['salt'],
-				 birthYear: 2020,
-			 },
-			 {
-				 name: 'Blobby',
-				 species: 'Bird?',
-				 favFoods: ['your soul'],
-				 birthYear: 2020,
-			 },
-			 {
-				 name: 'El Taco',
-				 species: 'Cat',
-				 favFoods: ['tequila', 'bar snax'],
-				 birthYear: 1995,
-			 },
-			 {
-				 name: 'Nibbles',
-				 species: 'Hungry Cat',
-				 favFoods: ['blankets', 'feet', 'tinsel'],
-				 birthYear: 2003,
-			 },
-			 {
-				 name: 'BoopySnoot',
-				 species: 'Tiny Cat',
-				 favFoods: ['milk'],
-				 birthYear: 2021,
-			 },
-			 {
-				name: "Captain Catface",
-				species: "Sea Cattain",
-				favFoods: ["fish, rum"],
-				birthYear: 2016,
-			  }
-	 ]);
+	const [cats,setCats] = useState<Array<Cat>>(catData);
 	 const catCount = cats.length;
 	 console.log("Our pretties 😻: ",cats);
 
@@ -101,7 +25,9 @@ function App() {
 			<main>
 		    <div className='cards__wrapper'>
              {(cats.map((cat,index) =>{
-				 return <CatCard name ={cat.name} 
+				 return <CatCard 
+				 key ={cat.id}
+				 name ={cat.name} 
 				 species ={cat.species} 
 				 favFoods = {cat.favFoods} 
 				 birthYear={cat.birthYear}
--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -115,6 +115,7 @@ const images = [
 
 
 interface CatCardProps {
+    
     name: string;
     species: string;
     favFoods:Array<string>;
@@ -125,8 +126,9 @@ interface CatCardProps {
 }
 
 
-const CatCard : React.FC<CatCardProps>  = ({name,species,favFoods,birthYear,catIndex})=>
+const CatCard : React.FC<CatCardProps>  = ({ name,species,favFoods,birthYear,catIndex})=>
     <div className = "card">
+        
         <h3 className="card__text card__header">{name}</h3>
      { catIndex <images.length &&(<CatImage image ={images[catIndex].image}  
         altText ={images[catIndex].altText}
@@ -134,6 +136,7 @@ const CatCard : React.FC<CatCardProps>  = ({name,species,favFoods,birthYear,catI
         licenceUrl ={images[catIndex].licenceUrl}
          attributionName={images[catIndex].attributionName} 
          attributionUrl={images[catIndex].attributionUrl}/>)}
+        
         <p className="card__text">Species: {species}</p>
         <p className="card__text">Favourite Food(s): {favFoods}</p>
         <p className="card__text">Birth Year: {birthYear}</p>
